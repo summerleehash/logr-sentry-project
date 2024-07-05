f45c6744c52d7191665c8125ad24eb18c29b3907
Summer Fores <colin.capurso@enabled.com.au>
2024-07-05 11:51:42 +0930

    Added firebase client and auth login check

--- webserver/functions/src/config.ts
+++ webserver/functions/src/config.ts
@@ -1,4 +1,27 @@
 import firebaseAdmin from "firebase-admin";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
 
-export const admin = firebaseAdmin.initializeApp()
-;
+export const admin = firebaseAdmin.initializeApp();
+
+const getFirebaseConfig = async () => {
+  const snap = await admin.firestore().collection("configuration").doc("firebase-config").get()
+  return snap.data() as {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+  }
+}
+
+let app: FirebaseApp | null = null
+
+export const getFirebaseClientApp = async () => {
+  if (!app) {
+    app = initializeApp(await getFirebaseConfig())
+  }
+  
+  return app
+}
--- webserver/functions/src/logr-sentry-web/logr-sentry-web.ts
+++ webserver/functions/src/logr-sentry-web/logr-sentry-web.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { onRequest } from "firebase-functions/v2/https";
 import mustacheExpress from "mustache-express";
 import { ClusterFirestoreService } from "./Cluster";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirebaseClientApp } from "../config";
 
 const app = express();
 
@@ -21,6 +23,45 @@ app.get("/", async (req, res) => {
   const clusters = await clusterService.getClusters();
   res.render("index", { clusters, sort: "asc" });
 });
+
+app.get("/login", async (req, res) => {
+  const clusters = await clusterService.getClusters();
+  res.render("login", { clusters, sort: "asc" });
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body as { email?: string, password?: string };
+
+    if (!email || email.trim().length === 0) {
+      throw new Error("Email is required");
+    }
+
+    if (!password || password.trim().length === 0) {
+      throw new Error("Password is required");
+    }
+    
+    const auth = getAuth(await getFirebaseClientApp())
+    const result = await signInWithEmailAndPassword(auth, email, password)
+
+    if (result.user) {
+      // TODO: Save session and then redirect
+      // result.user.uid
+
+      res.redirect("/");
+      return;
+    }
+
+    throw new Error("Invalid email or password");    
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
+    res.render("login", { error: error.message });
+  }
+})
+
+// TODO: POST /login
+// TODO: GET /logout
+
 
 app.get("/test", async (req, res) => {
   res.json({ result: 'ok' });
@@ -47,3 +88,4 @@ app.post("/api/clusters", async (req, res) => {
 });
 
 export const server = onRequest({ cors: true }, app);
+
